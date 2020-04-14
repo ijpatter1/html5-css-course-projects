442f81e6cac5fef0e2fd789ad8d804454088b62b
ijpatter1 <ian@tunameltsmyheart.com>
2020-04-14 15:30:11 -0400

    Add basic-html-and-javascript1
    Added basic-html-and-javascript folder to course project repository

---
+++ basic-html-and-javascript1/basic-javascript-1.js
@@ -0,0 +1,6 @@
+alert("Hello World! I am an alert box!");
+function myFirstFunction() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result
+}
